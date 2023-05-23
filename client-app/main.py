76fb893c9b40ae8c03c56f50df60256e27f0dd2d
Steven <steven.kollo.workspace@gmail.com>
2023-05-22 20:17:41 -0400

    Upload request

--- client-app/main.py
+++ client-app/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, render_template
+from flask import Flask, flash, request, make_response, render_template
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 
@@ -15,26 +15,19 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/uploads/<name>')
-def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+@app.route('/upload', methods=['POST'])
+def upload():
+
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return make_response('', 200)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(request.url)
+@app.route('/', methods=['POST', 'GET'])
+def render():
     return render_template('index.html')
--- client-app/static/index.js
+++ client-app/static/index.js
@@ -2,8 +2,52 @@ function selectPeriod() {
     document.getElementById(`select-period`).style = "display: none;"
     document.getElementById(`upload`).style = "display: inline;"
 }
-
-
+$(document).ready(function () {
+    // Register the listener for submit events.
+    $('form[name="upload-form"]').submit(function (evt) {
+        // Prevent the form from default behavior.
+        evt.preventDefault()
+        // Serialize the form data. The entire form is passed as a parameter.
+        const formData = new FormData($(this)[0])
+        // Send the data via ajax.
+        $.ajax({
+            type: 'post',
+            url: `${getCurrentURL()}/upload`,
+            data: formData,
+            contentType: false,
+            cache: false,
+            processData: false,
+        }).done(function (data) {
+            console.log('success')
+        }).fail(function (xhr, status, error) {
+            console.error('error')
+        });
+    });
+});
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();    // prevent page from refreshing
+//     const formData = new FormData(form);  // grab the data inside the form fields
+//     fetch('/upload', {   // assuming the backend is hosted on the same server
+//         method: 'POST',
+//         body: formData,
+//     }).then(function(response) {
+//         // do something with the response if needed.
+//         // If you want the table to be built only after the backend handles the request and replies, call buildTable() here.
+//     });
+// });
 function importFile() {
+    // fetch(`${getCurrentURL()}/upload`, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({ "id": 78912 })
+    // })
+    //     .then(response => console.log(response))
+    // .then(response => console.log(JSON.stringify(response)))
+}
 
+function getCurrentURL() {
+    return window.location.href
 }
