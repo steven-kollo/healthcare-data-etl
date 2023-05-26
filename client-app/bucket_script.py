62110220189556e7b6518fff55ba9cefbeb1c29b
Steven <steven.kollo.workspace@gmail.com>
2023-05-25 21:52:57 -0400

    GCP upload read file without saving

--- client-app/bucket_script.py
+++ client-app/bucket_script.py
@@ -4,7 +4,7 @@ from google.cloud import storage
 
 def upload_blob(file_name, file):
     bucket_name = 'healthcare-data-bucket'
-    contents = file.read()  # string file here
+    contents = file  # string file here
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
