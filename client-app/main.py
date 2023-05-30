6449d2658be1c8d66b8adc5f6ddb19350136db68
Steven <steven.kollo.workspace@gmail.com>
2023-05-29 20:42:49 -0400

    python subproces

--- client-app/main.py
+++ client-app/main.py
@@ -1,26 +1,11 @@
 from flask import Flask, flash, request, abort, make_response, render_template
 from werkzeug.utils import secure_filename
 from bucket_script import upload_blob
-import os
 import subprocess
-from subprocess import Popen, PIPE
-from subprocess import check_output
+
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'csv', 'xls', 'xlsm', 'xlsx'}
-
-
-def get_shell_script_output_using_communicate():
-    session = Popen(['./trigger_airflow.sh'], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = session.communicate()
-    if stderr:
-        raise Exception("Error "+str(stderr))
-    return stdout.decode('utf-8')
-
-
-def get_shell_script_output_using_check_output():
-    stdout = check_output(['./trigger_airflow.sh']).decode('utf-8')
-    return stdout
 
 
 def allowed_file(filename):
@@ -31,19 +16,12 @@ def allowed_file(filename):
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# TODO Trigger Airflow from the bucket, not app
-
 
 @app.route('/trigger_airflow', methods=['GET'])
 def trigger():
     command = 'touch test.txt'
-    subprocess.check_output(
-        [command], shell=True)
-    # o = subprocess.run(
-    #     ["./trigger_airflow.sh $ROOT_FOLDER_ID"],
-    #     stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
-    # )
-    # get_shell_script_output_using_check_output()
+    subprocess.run([command], shell=True)
+
     return make_response('nice', 200)
 
 
@@ -60,7 +38,6 @@ def upload():
         formated_filename = f'{label}_{period}.{filename.rsplit(".", 1)[1].lower()}'
         upload_blob(file_name=formated_filename,
                     file=file)
-        # get_shell_script_output_using_check_output()
         return make_response(file.read(), 200)
 
 
